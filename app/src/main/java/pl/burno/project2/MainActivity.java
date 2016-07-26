fbf5758c88985181d7fa51cebd513e96e597d75b
burnO <bruno.wieczorek@gmail.com>
2016-07-26 12:07:27 +0200

    git test

--- app/src/main/java/pl/burno/project2/MainActivity.java
+++ app/src/main/java/pl/burno/project2/MainActivity.java
@@ -9,6 +9,7 @@ public class MainActivity extends AppCompatActivity
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
+        //git test
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
