01225e826a33fa39ec5ea98ee7633a592fc1a6d2
burnO <bruno.wieczorek@gmail.com>
2016-07-26 12:40:56 +0200

    Email layout 1

--- app/src/main/java/pl/burno/project2/MainActivity.java
+++ app/src/main/java/pl/burno/project2/MainActivity.java
@@ -5,11 +5,9 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity
 {
-
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
-        //git test
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
